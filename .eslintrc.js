a7147586a5cfc3f958b0dd7b02f0c8e529c27c99
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-01-31 12:55:17 +0800

    feat: add linting support (airbnb) into template

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    extends: [
+        'airbnb-base', // Airbnb's base JavaScript style guide
+    ],
+    env: {
+        browser: true, // Enable browser global variables
+        node: true,   // Disable Node.js global variables
+        es6: true,     // Enable ES6 features
+    },
+    parserOptions: {
+        ecmaVersion: 2021, // Use latest ECMAScript version
+        sourceType: 'module', // Allow ES modules
+    },
+    rules: {
+        // Add custom rules/overrides here if needed
+        'no-console': 'off', // Allow console.log (common in development)
+        'import/prefer-default-export': 'off', // Allow single named exports
+        'no-plusplus': ['error', { allowForLoopAfterthoughts: true }], // Allow i++ in for loops
+    },
+};
